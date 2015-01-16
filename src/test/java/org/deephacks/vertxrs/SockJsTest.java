7b70f4595fdf17acfbaa271e973eebbd3c1b177f
Kristoffer Sjogren <stoffe@gmail.com>
2015-01-17 00:17:15 +0100

    small lambdafication

--- src/test/java/org/deephacks/vertxrs/SockJsTest.java
+++ src/test/java/org/deephacks/vertxrs/SockJsTest.java
@@ -19,12 +19,9 @@ public class SockJsTest extends BaseTest {
     Thread thread = Thread.currentThread();
     AtomicReference<String> result = new AtomicReference<>();
 
-    eventBus.send("test", "hello", new Handler<Message<String>>() {
-      @Override
-      public void handle(Message<String> event) {
-        result.set(event.body());
-        LockSupport.unpark(thread);
-      }
+    eventBus.send("test", "hello", (Message<String> event) -> {
+          result.set(event.body());
+          LockSupport.unpark(thread);
     });
     LockSupport.park(thread);
     assertThat(result.get(), is("{\"value\":\"hello\"}"));
