db14a27d18b1081719b2b2147f8ad3c168d70d45
Kristoffer Sjogren <stoffe@gmail.com>
2015-01-16 19:04:28 +0100

    exception mapper test

--- src/test/java/org/deephacks/vertxrs/ExceptionMapperTest.java
+++ src/test/java/org/deephacks/vertxrs/ExceptionMapperTest.java
@@ -1,4 +1,63 @@
 package org.deephacks.vertxrs;
 
-public class ExceptionMapperTest {
+
+import org.jboss.resteasy.spi.ApplicationException;
+import org.junit.Test;
+
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.ServiceUnavailableException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+import com.squareup.okhttp.Response;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ExceptionMapperTest extends BaseTest {
+
+  @Test
+  public void testExceptionMapper() throws IOException {
+    Response response = GET("/exception-mapper/exception");
+    assertThat(response.code(), is(400));
+  }
+
+  @Provider
+  public static class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Throwable> {
+
+    @Context
+    private org.jboss.resteasy.spi.HttpRequest request;
+
+    @Context
+    private org.jboss.resteasy.spi.HttpResponse response;
+
+    @Override
+    public javax.ws.rs.core.Response toResponse(Throwable exception) {
+      if (exception instanceof IllegalArgumentException) {
+        return javax.ws.rs.core.Response.status(400).build();
+      } else if (ServiceUnavailableException.class.isAssignableFrom(exception.getClass())) {
+        return javax.ws.rs.core.Response.status(503).build();
+      } else if (ForbiddenException.class.isAssignableFrom(exception.getClass())) {
+        return javax.ws.rs.core.Response.status(403).build();
+      } else if (RuntimeException.class.isAssignableFrom(exception.getClass())) {
+        return javax.ws.rs.core.Response.serverError().build();
+      }
+      return null;
+    }
+  }
+
+
+  @Path("/exception-mapper")
+  public static class ExceptionMapperResource {
+
+    @GET
+    @Path("exception")
+    public void exception() {
+      throw new IllegalArgumentException();
+    }
+  }
 }
--- src/test/java/org/deephacks/vertxrs/Resteasy.java
+++ src/test/java/org/deephacks/vertxrs/Resteasy.java
@@ -1,7 +1,7 @@
 package org.deephacks.vertxrs;
 
-import org.jboss.resteasy.core.Dispatcher;
 import org.jboss.resteasy.spi.ResteasyDeployment;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
@@ -9,16 +9,19 @@ import java.util.Set;
 
 public class Resteasy extends Application {
   private final Set<Object> services = new HashSet<>();
-  private Dispatcher dispatcher;
   private ResteasyDeployment deployment = new ResteasyDeployment();
 
   public Resteasy(Object... services) {
     this.services.add(new TestResource());
     this.services.add(new AsyncJaxrsResource());
     this.services.add(new Resource());
+    this.services.add(new ExceptionMapperTest.ExceptionMapperResource());
     for (Object service : services) {
       this.services.add(service);
     }
+    ResteasyProviderFactory factory = ResteasyProviderFactory.getInstance();
+    factory.registerProviderInstance(new ExceptionMapperTest.ExceptionMapper());
+    deployment.setProviderFactory(factory);
   }
 
   @Override
