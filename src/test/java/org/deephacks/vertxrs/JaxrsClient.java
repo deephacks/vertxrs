966e7891d6a29a36dcd1b20e2a76553be564644d
Kristoffer Sjogren <stoffe@gmail.com>
2015-06-11 18:25:36 +0200

    Revert "removed commons httpclient in favor for okhttp"
    This reverts commit 2f35da71dc6888186adeed9dee23941475399388.

--- src/test/java/org/deephacks/vertxrs/JaxrsClient.java
+++ src/test/java/org/deephacks/vertxrs/JaxrsClient.java
@@ -1,7 +1,6 @@
 package org.deephacks.vertxrs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.squareup.okhttp.OkHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
@@ -12,17 +11,24 @@ import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 import javax.ws.rs.ext.ContextResolver;
 
 public class JaxrsClient {
-  private static final OkHttpClient okHttpClient = new OkHttpClient();
-  private static final ObjectMapper objectMapper = new ObjectMapper();
-  private static final JacksonContextResolver jacksonContextResolver =
-    new JacksonContextResolver(objectMapper);
+
+  private static final ApacheHttpClient4Engine httpClient;
+  private static final JacksonContextResolver jacksonContextResolver;
+
+  static {
+    PoolingClientConnectionManager connectionManager = new PoolingClientConnectionManager();
+    connectionManager.setDefaultMaxPerRoute(connectionManager.getMaxTotal());
+    httpClient = new ApacheHttpClient4Engine(new DefaultHttpClient(connectionManager));
+    ObjectMapper objectMapper = new ObjectMapper();
+    jacksonContextResolver = new JacksonContextResolver(objectMapper);
+  }
 
   public static <T> T newClient(Class<T> cls, Config config) {
     ResteasyClient client = new ResteasyClientBuilder()
-      .httpEngine(new OkHttpClientEngine(okHttpClient))
-      .register(jacksonContextResolver)
-      .build();
-    ResteasyWebTarget target = client.target(config.getHttpHostPortUrl(""));
+            .httpEngine(httpClient)
+            .register(jacksonContextResolver)
+            .build();
+    ResteasyWebTarget target = (ResteasyWebTarget) client.target(config.getHttpHostPortUrl(""));
     return target.proxy(cls);
   }
 
